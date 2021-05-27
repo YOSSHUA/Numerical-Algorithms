0f9366f3a002c85ea6cdf5b5506aefc55f7b529a
Yosshua Cisneros <42878951+YOSSHUA@users.noreply.github.com>
2021-05-26 22:01:58 -0500

    Update and rename a1_muestreoBernoulli.m to sampleBernoulli.m

--- sampleBernoulli.m
+++ sampleBernoulli.m
@@ -1,6 +1,4 @@
-%A tiene en cada columna la proporcion de valores que son <= p y tienen
-%tamaño j*K. Después vemos cómo se comporta la suma de estas(1 si es menor
-%que p y 0 en c.o.c) 
+%Code to see the distribution of the average sample.
 M = 20; %No. reng
 N = 80; % No. cols
 K = 10; %Aumento de la cantidad de puntos
